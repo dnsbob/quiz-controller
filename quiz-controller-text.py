00cd02131b1d299d87cf973e241e4457891a4588
Bob Harold <bgithub@halfgoat.com>
2023-01-15 21:16:27 -0500

    reads keyboard, waits for usb port

--- quiz-controller-text.py
+++ quiz-controller-text.py
@@ -3,7 +3,10 @@
 quiz controller, text version
 '''
 
+############# ***** NEXT - DECIPHER CONTROLLER INPUT ****
+
 import time
+import serial
 
 # from https://stackoverflow.com/questions/2408560/non-blocking-console-input
 import sys
@@ -27,6 +30,34 @@ class NonBlockingConsole(object):
             return sys.stdin.read(1)
         return False
 
+
+class Usbserial(object):
+    def __enter__(self):
+        # serial setup
+        SERIALPORT = "/dev/ttyACM0"
+        BAUDRATE = 115200
+        while True:
+            try:
+                self.ser = serial.Serial(SERIALPORT, BAUDRATE)
+                print("\n\n")
+                return self
+            except FileNotFoundError as e:
+                print(f"waiting on port {SERIALPORT}:{e}",end="\r")
+            except serial.serialutil.SerialException as e:
+                print(f"waiting for {SERIALPORT}:{e}",end="\r")
+            time.sleep(1)
+        
+
+    def __exit__(self, type, value, traceback):
+        self.ser.close()
+
+    def get_data(self):
+        cnt=self.ser.in_waiting
+        if cnt:
+            return self.ser.readline(cnt)
+        return None
+
+
 def main():
     '''quiz-controller-text'''
     # setup
@@ -36,48 +67,54 @@ def main():
     enable=[]   # enable status of player
     for i in range(max):
         print("player",i,"is key",keys[i])
-        player.append(0)
+        player.append(False)    # false=seated, true=standing
         enable.append(True)
+
+
 
     # test
     player[3]=1
 
     # main loop
     with NonBlockingConsole() as nbc:
-        while True:
-            # print
-            print("\r",end="")
-            for i in range(max):
-                playernum=i+1
-                if enable[i]:
-                    if player[i]:
-                        symbol="*"
+        with Usbserial() as myusb:
+            while True:
+                # read controller
+                c=myusb.get_data()
+                if c:
+                    print(f"from controller:{c}:")
+                # print
+                print("\r",end="")
+                for i in range(max):
+                    playernum=i+1
+                    if enable[i]:
+                        if player[i]:
+                            symbol="*"
+                        else:
+                            symbol=" "
                     else:
-                        symbol=" "
-                else:
-                    symbol="_"
-                print(f" {symbol}{playernum}{symbol} ",end="")
-            #print()
-            # read keyboard
-            c=nbc.get_data()
-            if c:
-                try:
-                    j=keys.index(c)
-                except ValueError as e:
-                    print(f"key {c} not understood")
-                    c=""
-            if c:
-                if j<max:
-                    # toggle seat enable/disable
-                    if enable[j]:
-                        enable[j]=False
-                    else:
-                        enable[j]=True
-            time.sleep(.1)
-
-
-
-
+                        symbol="_"
+                    print(f" {symbol}{playernum}{symbol} ",end="")
+                #print()
+                # read keyboard
+                c=nbc.get_data()
+                if c:
+                    try:
+                        j=keys.index(c)
+                    except ValueError as e:
+                        print(f"key {c} not understood")
+                        c=""
+                if c:
+                    if j<max:
+                        # toggle seat enable/disable
+                        if enable[j]:
+                            enable[j]=False
+                        else:
+                            enable[j]=True
+                    elif j<2*max:
+                        # pretend seat went True
+                        player[j-max]=True
+                time.sleep(.1)
 
 
 if __name__ == "__main__":
